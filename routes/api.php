561f4a44a3449afd8eeb5f96b7268d45324fb741
Catiflam <gilfrid1@gmail.com>
2023-11-04 11:07:41 +0100

    new db

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
 
+use App\Models\Project;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::get('/projects', [ProjectController::class, 'index']); 
+Route::apiResource('projects', ProjectController::class)->only(['index','show']);
